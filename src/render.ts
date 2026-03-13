56ec177b80dcf6b11edaeb526ee590dd81a0be6c
Thomas-Slot <th.slot@outlook.com>
2026-03-13 20:42:41 +0100

    Update 2

--- src/render.ts
+++ src/render.ts
@@ -1,9 +1,18 @@
 import { state } from "./state";
+import { elements } from "./elements";
 
 export function render() {
     console.log('rendering...');
 
     state.items.forEach((item) => {
+        const li = document.createElement("li");
+        li.className = "todo-item";
+
+        elements.gridItems.appendChild(li);
+
+        const input = document.createElement("input");
+
+        input.type = "checkbox";
         
     })
 }
